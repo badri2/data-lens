6a0a53fef3c5e30b36a21e2633f31bebe9f1d98a
Badri Mangalam <badri.mangalam@gmail.com>
2017-05-18 08:06:59 -0700

    Moved data-lens to its own repo

---
+++ src/main/java/com/wrs/datamgmt/DataLensApplication.java
@@ -0,0 +1,12 @@
+package com.wrs.datamgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLensApplication.class, args);
+	}
+}
